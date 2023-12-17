a5a9043569214ca55838c0d6259b45a665561266
BuzZz <79199175856@yandex.ru>
2023-12-17 22:47:30 +0300

    add method docs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,5 +47,11 @@ return {
   },
   {
      'folke/neodev.nvim'
-  }
+  },
+    {
+  "ray-x/lsp_signature.nvim",
+  event = "VeryLazy",
+  opts = {},
+  config = function(_, opts) require'lsp_signature'.setup(opts) end
+}
 }
